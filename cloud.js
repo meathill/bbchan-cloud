381859c2a2432a8dad278ea116a1925db2f08943
Meathill <meathill@gmail.com>
2021-06-17 22:07:07 +0800

    change: added `drawWinner`

--- cloud.js
+++ cloud.js
@@ -1,17 +1,59 @@
-const AV = require('leanengine')
-const fs = require('fs')
-const path = require('path')
+const {STATUS_COMPLETED} = require("./model/lucky");
+const {
+  Cloud,
+  Object,
+  Query,
+} = require('leanengine');
 
-/**
- * Loads all cloud functions under the `functions` directory.
- */
-fs.readdirSync(path.join(__dirname, 'functions')).forEach( file => {
-  require(path.join(__dirname, 'functions', file))
-})
+Cloud.define('drawWinner', async function(req) {
+  const {
+    currentUser,
+    params: {
+      luckyId,
+    },
+  } = req;
+  const model = 'danmu'
+  const roomId = currentUser.get('roomId');
+  const lucky = Object.createWithoutData(model, luckyId);
+  await lucky.fetch();
+  const {
+    startTime,
+    endTime,
+    strict,
+    content,
+    onlyOnce,
+    number,
+  } = lucky.toJSON();
+  const query = new Query(model);
+  query.equalTo('room', roomId)
+    .greaterThanOrEqualTo('ts', new Date(startTime).getTime() / 1000 >> 0)
+    .lessThanOrEqualTo('ts', new Date(endTime).getTime() / 1000 >> 0)
+    .exists('uid');
+  if (strict) {
+    query.equalTo('content', content);
+  } else {
+    query.contains('content', content);
+  }
+  let danmu = await query.find();
+  if (onlyOnce) {
+    danmu = Object.values(danmu.reduce((memo, item) => {
+      const uid = item.get('uid');
+      memo[uid] = item;
+      return memo;
+    }, {}));
+  }
+  let winners = new Set();
+  while (winners.size < number) {
+    const index = danmu.length * Math.random() >> 0;
+    winners.add(index);
+  }
+  winners = [...winners].map(winner => danmu[winner]);
+  lucky.set('winners', winners);
+  lucky.set('status', STATUS_COMPLETED);
+  await lucky.save();
 
-/**
- * A simple cloud function.
- */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!'
-})
+  return {
+    status: 0,
+    winners,
+  };
+});
---
+++ model/lucky.js
@@ -0,0 +1,40 @@
+import { Object } from 'leancloud-storage';
+
+export const LUCKY = 'lucky';
+
+export const STATUS_NORMAL = 0;
+export const STATUS_CANCELED = 50;
+export const STATUS_COMPLETED = 100;
+export const STATUS_LABEL = {
+  [STATUS_NORMAL]: '进行中',
+  [STATUS_CANCELED]: '已取消',
+  [STATUS_COMPLETED]: '已完成',
+};
+export const STATUS_STYLE = {
+  [STATUS_NORMAL]: 'text-primary fw-bold fs-5',
+  [STATUS_CANCELED]: 'text-muted',
+  [STATUS_COMPLETED]: 'text-success',
+}
+
+export function formatLuck(model) {
+  const {
+    objectId,
+    createdAt,
+    updatedAt,
+    owner,
+    ...json
+  } = model.toJSON();
+  return {
+    ...json,
+    statusLabel: STATUS_LABEL[json.status],
+    statusClass: STATUS_STYLE[json.status],
+  };
+}
+
+class Lucky extends Object {
+
+}
+
+Object.register(Lucky, LUCKY);
+
+export default Lucky;
